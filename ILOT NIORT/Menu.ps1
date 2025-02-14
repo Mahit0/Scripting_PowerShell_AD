be13720a88b6ba6deee8c2819f40b2cfc2a383e5
Romain <73798211+leromaindu79@users.noreply.github.com>
2025-02-14 13:45:08 +0100

    Update Menu.ps1
    Ajout de ma signature

--- ILOT NIORT/Menu.ps1
+++ ILOT NIORT/Menu.ps1
@@ -1,27 +1,34 @@
+#########################################
+# Author : Romain GUYOT                 #
+# Name : .\menu.ps1                     #
+# Date : 14/02/2025                     #
+# Version : 0.1                         #
+#########################################
+    
     #Options
     Write-Host "Menu Active Directory"
     
-    Write-Host "1. Création d'utilisateur"
-    Write-Host "2. Création de groupe"
-    Write-Host "3. Création d'OU"
+    Write-Host "1. CrÃ©ation d'utilisateur"
+    Write-Host "2. CrÃ©ation de groupe"
+    Write-Host "3. CrÃ©ation d'OU"
     Write-Host "4. Description d'un groupe"
-    Write-Host "5. Associer un utilisateur à un groupe"
+    Write-Host "5. Associer un utilisateur Ã  un groupe"
     Write-Host "6. Modifier le Mot de Passe d'un utilisateur"
-    Write-Host "7. Désassocier un utilisateur à un groupe"
+    Write-Host "7. DÃ©sassocier un utilisateur Ã  un groupe"
     Write-Host "8. Suppression d'utilisateur"
     Write-Host "9. Supression de groupe"
     Write-Host "10. Suppression d'OU"
 
     #Choix
-    $choix = Read-Host "Tapez le numéro de l'option" 
+    $choix = Read-Host "Tapez le numÃ©ro de l'option" 
         switch ($choix){
-        1 { .\scripts\1. Création d'utilisateur.ps1 }
-        2 { .\scripts\2. Création de groupe.ps1 }
-        3 { .\scripts\3. Création d'OU.ps1 }
+        1 { .\scripts\1. CrÃ©ation d'utilisateur.ps1 }
+        2 { .\scripts\2. CrÃ©ation de groupe.ps1 }
+        3 { .\scripts\3. CrÃ©ation d'OU.ps1 }
         4 { .\scripts\4. Description d'un groupe.ps1 }
-        5 { .\scripts\5. Associer un utilisateur à un groupe.ps1 }
+        5 { .\scripts\5. Associer un utilisateur Ã  un groupe.ps1 }
         6 { .\scripts\6. Modifier le Mot de Passe d'un utilisateur.ps1 }
-        7 { .\scripts\7. Désassocier un utilisateur à un groupe.ps1 }
+        7 { .\scripts\7. DÃ©sassocier un utilisateur Ã  un groupe.ps1 }
         8 { .\scripts\8. Suppression d'utilisateur.ps1 }
         9 { .\scripts\9. Supression de groupe.ps1 }
        10 { .\scripts\10. Suppression d'OU.ps1 }
