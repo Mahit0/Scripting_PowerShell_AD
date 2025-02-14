09ce5e37fad46282ef2a0af7f78642d3bb039ba9
Matteo <64864491+Mahit0@users.noreply.github.com>
2025-02-14 12:28:46 +0100

    Update info+suppr_ordi.ps1

--- Script Finaux/info+suppr_ordi.ps1
+++ Script Finaux/info+suppr_ordi.ps1
@@ -1,9 +1,45 @@
 #fonction pour connaitre les informations d'un ou plusieurs ordinateurs
 function func_computer_information {
-    $computer = Read-Host "Entrez le nom de l'ordinateur"
-    Get-ADComputer -Filter "name -like '*$computer*'" -Properties * | Select-Object DistinguishedName, Name, Description, IPV4Address | Format-List
-    Pause
-    Func_PC
+    do {
+    $Menu = Read-Host "Veuillez faire un choix
+    1) Afficher la liste de toutes les machines
+    2) Recherche par nom
+    3)Quit
+    "
+    switch ($Menu) {
+        1 { 
+            #Affiche tout les noms d'ordinateur
+            Get-ADComputer -Filter * -Properties * | Select-Object name 
+         }
+        2 {
+            do {
+                #Affiiche la demande de saisie
+                $saisie = Read-Host "Entrer le nom de l'ordinateur rechercher"
+                #Verifie si la saisie existe
+                $saisieValide = Get-ADComputer -Filter "name -eq '$saisie'"
+                if ($saisieValide) {
+                    #Affiche les informations nom, date de creation, Description...
+                    Get-ADComputer -Filter * -Properties * | Select-Object name, Created, Description, OperatingSystem, LastLogonDate, MemberOf | Where-Object name -eq $saisie
+                    break
+                }
+                else {
+                    Write-Host "Ordinateur inexistant" -ForegroundColor red
+                    Start-sleep 1
+                    continue
+                }
+            } while ($saisie)
+        }
+        3 {
+            Write-Host "Au revoir"
+            Start-Sleep 2
+            exit
+        }
+        Default {
+            Write-Host "Erreur de Saisie" -ForegroundColor red
+            Start-Sleep 1
+        }
+    }
+} while ($Menu -notmatch '^[1-3]')
 }
 
 #fonction pour supprimer un ordinateur
